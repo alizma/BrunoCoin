51f8453eef0d8ba2e17caa4b9024e0bf22dad6f5
alizma <alizma@brown.edu>
2021-06-02 23:56:04 -0400

    added test HndlTxReqNotEnough

--- test/wallet_tests.go
+++ test/wallet_tests.go
@@ -4,6 +4,7 @@ import (
 	"BrunoCoin/pkg"
 	"BrunoCoin/pkg/utils"
 	"testing"
+	"time"
 )
 
 func HndlTxReqAmtZero(t *testing.T) {
@@ -16,4 +17,34 @@ func HndlTxReqAmtZero(t *testing.T) {
 	genNd.StartMiner()
 
 	genNd.SendTx(0, 0, node2.Id.GetPublicKeyBytes())
+
+	ChkMnChnCons(t, []*pkg.Node{genNd, node2})
+	ChkNdPrs(t, genNd, []*pkg.Node{node2})
+	ChkNdPrs(t, node2, []*pkg.Node{genNd})
+
+	ChkTxSeenLen(t, genNd, 0)
+	ChkTxSeenLen(t, node2, 0)
+}
+
+func HndlTxReqNotEnough(t *testing.T) {
+	utils.SetDebug(true)
+
+	genNd := NewGenNd()
+	node2 := pkg.New(pkg.DefaultConfig(GetFreePort()))
+	genNd.Conf.MnrConf.InitPOWD = utils.CalcPOWD(1)
+	genNd.Start()
+	genNd.StartMiner()
+
+	genNd.SendTx(200, 0, node2.Id.GetPublicKeyBytes())
+
+	time.Sleep(5 * time.Second)
+
+	node2.SendTx(999999, 0, genNd.Id.GetPublicKeyBytes())
+
+	ChkMnChnCons(t, []*pkg.Node{genNd, node2})
+	ChkNdPrs(t, genNd, []*pkg.Node{node2})
+	ChkNdPrs(t, node2, []*pkg.Node{genNd})
+
+	ChkTxSeenLen(t, genNd, 0)
+	ChkTxSeenLen(t, node2, 0)
 }
