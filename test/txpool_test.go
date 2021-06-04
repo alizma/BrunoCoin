699c161c2bbe13ffe75dfe885d002a3f7559e170
alizma <alizma@brown.edu>
2021-06-04 08:08:44 -0400

    TestAddMassiveFee

--- test/txpool_test.go
+++ test/txpool_test.go
@@ -25,3 +25,24 @@ func TestAddTxWthZeroFee(t *testing.T) {
 		t.Errorf("exp pri: 1, actual pri: %d", qooo.P)
 	}
 }
+
+func TestAddMassiveFee(t *testing.T) {
+	utils.SetDebug(true)
+	genNd := NewGenNd()
+	genNd.Start()
+
+	txpool := miner.NewTxPool(miner.DefaultConfig(-1))
+
+	tx := MakeSingleTx(genNd, genNd.Id.GetPublicKeyBytes(), 9999999)
+	txpool.Add(tx)
+
+	if txpool.Ct.Load() != 2 {
+		t.Errorf("expected cap: 2, actual cap: %d", txpool.Ct.Load())
+	}
+
+	qooo := (*txpool.TxQ)[0]
+	if qooo.P != miner.CalcPri(tx) {
+		t.Errorf("exp pri: 1, actual pri: %d", qooo.P)
+	}
+
+}
