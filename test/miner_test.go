93576702b666db82787e837b2aad0bb39b92efa4
alizma <alizma@brown.edu>
2021-06-04 07:51:17 -0400

    TestNilTxHndl

--- test/miner_test.go
+++ test/miner_test.go
@@ -37,5 +37,19 @@ func TestHndlActiveMnr(t *testing.T) {
 	if mnr.TxP.Ct.Load() != 1 {
 		t.Errorf("expected count is: %v, actual count is: %v", 1, mnr.TxP.Ct.Load())
 	}
+}
 
+func TestNilTxHndl(t *testing.T) {
+	utils.SetDebug(true)
+	genNd := NewGenNd()
+	genNd.Start()
+
+	mnr := miner.New(genNd.Mnr.Conf, genNd.Id)
+	mnr.Active.Store(false)
+
+	mnr.HndlTx(nil)
+
+	if mnr.TxP.Ct.Load() != 1 {
+		t.Errorf("expected count is: %v, actual count is: %v", 0, mnr.TxP.Ct.Load())
+	}
 }
