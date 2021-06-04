f0cc34a79a66c801d8f7a2de9b902bce8d67addc
alizma <alizma@brown.edu>
2021-06-04 08:18:27 -0400

    calcpri_test

---
+++ test/calcpri_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"BrunoCoin/pkg/miner"
+	"BrunoCoin/pkg/utils"
+	"testing"
+)
+
+func TestNilPriCalc(t *testing.T) {
+	bullshitpriority := miner.CalcPri(nil)
+	if bullshitpriority != 0 {
+		t.Errorf("expected 0, actual; %d", bullshitpriority)
+	}
+}
+
+func TestPriCalcTxiTxo(t *testing.T) {
+	utils.SetDebug(true)
+	genNd := NewGenNd()
+	genNd.Start()
+
+	minitx := MakeSingleTx(genNd, genNd.Id.GetPublicKeyBytes(), 2)
+
+	calculatedpri := miner.CalcPri(minitx)
+
+	if calculatedpri != (minitx.SumInputs()-minitx.SumOutputs())*100.0/minitx.Sz() {
+		t.Errorf("expected %d, actual; %d", calculatedpri, (minitx.SumInputs()-minitx.SumOutputs())*100.0/minitx.Sz())
+
+	}
+}
